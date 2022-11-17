a220cfa970cfec46d4fe171aa82d2e3401e19be5
Lucas0316 <hrmhrm087@gmail.com>
2022-11-17 00:53:39 -0800

    revised shell

--- shell.h
+++ shell.h
@@ -1,101 +1,62 @@
-#ifndef SHELL_H
-#define SHELL_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef _SHELL_H_
+#define _SHELL_H_
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <stdarg.h>
-#include <stdint.h>
-#define BUFF_SIZE 1024
-#define PROMPT "$ "
-/* error messages */
-#define EINVAL "Invalid argument"
-#define ENOMEM "Out of memory"
-#define ERROR "Error"
-#define WRONG "Something went wrong"
-#define END "exit"
-/**
- * struct built_in - list of builtins
- * @bi: The specifier
- * @f: The function associated with printing
- */
-typedef struct built_in
-{
-	char *bi;
-	int (*f)();
-} builtin_t;
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-extern char **environ;
-/* lists.c */
-size_t list_len(list_t *h);
-list_t *add_node(list_t **head, char *str);
-list_t *add_node_end(list_t **head, char *str);
-void free_list(list_t *head);
-list_t *get_node(list_t **head, char *str);
-/* prints.c */
-int _putchar(char c);
-void _strprint(char *str);
-void print_array(char **array);
-size_t print_list(list_t *h);
-/* strings.c */
-int _strlen(char *s);
-int _strncmp(char *s1, char *s2, int n);
-char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-/* more_strings */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/*==================================================*/
+/*============     Shell_Init       ==============*/
+/*==================================================*/
+
+int main(int ac, char **av, char **env);
+void prompt(void);
+void handle(int signals);
+void _EOF(char *buffer);
+void shell_exit(char **command);
+
+/*==================================================*/
+/*============     create_child       ==============*/
+/*==================================================*/
+
+void create_child(char **command, char *name, char **env, int cicles);
+int change_dir(const char *path);
+
+/*==================================================*/
+/*============        Execute       ==============*/
+/*==================================================*/
+
+void execute(char **command, char *name, char **env, int cicles);
+void print_env(char **env);
+char **_getPATH(char **env);
+void msgerror(char *name, int cicles, char **command);
+
+/*==================================================*/
+/*============          Tokening      ==============*/
+/*==================================================*/
+
+char **tokening(char *buffer, const char *s);
+
+/*==================================================*/
+/*============     Free Memory      ==============*/
+/*==================================================*/
+
+void free_dp(char **command);
+void free_exit(char **command);
+
+/*==================================================*/
+/*============  Auxiliar_Functions    ==============*/
+/*==================================================*/
+
 int _strcmp(char *s1, char *s2);
-char *_strchr(char *str, char c);
-int len_to_char(char *str, char c);
+unsigned int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-/* _strtok.c */
-int count_words(char *str, char delim);
-int _wrdlen(char *s, char delim);
-char **strtow(char *str, char delim);
-/*  array_list.c */
-int arr_size(char **arr);
-list_t *array_to_list(char **array);
-char **list_to_array(list_t **head);
-void free_array(char **array);
-/* new_env.c */
-char *_getenv(char *name, list_t **env_head);
-int _setenv(char *name, char *value, list_t **env_head);
-int _unsetenv(char *name, list_t **env_head);
-int delete_node(list_t **head, char *string);
-char *var_str(char *name, char *value);
-/* _getline.c */
-int _getline(char *input, int size);
-int exit_shell(char **line_tok);
-void clear_buffer(char *buffer);
-char *mem_cpy(char *dest, char *src, int n);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-/* cmd_line_loop.c */
-int cmd_line_loop(char *buffer, char *line, list_t **env_head);
-/* run_command */
-char **path_dirs_array(list_t **env_head);
-char *cmd_in_path(char *str, list_t **env_head);
-int run_command(char **line, list_t **env_head);
-/* built_ins.c */
-int built_ins(char **input, list_t **env_head);
-int exit_bi(char **line);
-int print_env(char **line, list_t **env_head);
-int set_env(char **line, list_t **env_head);
-int unset_env(char **line, list_t **env_head);
-#endif
+char *_strcat(char *dest, char *src);
+
+/*============ END      ==============*/
+
+#endif /* _SHELL_H_ */
