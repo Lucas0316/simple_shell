29d83be7e414e22478267b512b0f3c667b06f1d7
Lucas0316 <hrmhrm087@gmail.com>
2022-11-17 23:11:45 -0800

    final simple shell

--- execute.c
+++ execute.c
@@ -1,49 +1,92 @@
-#include "holberton.h"
+#include "hsh.h"
+
 /**
- * execute_proc - similar to puts in C
- * @cmd: a pointer the integer we want to set to 402
- *
- * Return: int
- */
-void execute_proc(char **cmd)
+  * execute - execute a command
+  * @info: arguments passed
+  *
+  * Return: status
+  */
+int execute(info_t *info)
 {
+	const builtin_t *builtin = get_builtin(*info->tokens);
 
-	char *parametro = (*(cmd + 1));
-	char *s, *slash = "/";
-	char *o;
-
-	char *vartoprint = *cmd;
-	char *argv[4];
-
-	if ((access(cmd[0], F_OK) == 0))
+	if (builtin)
 	{
-		argv[0] = cmd[0];
-		argv[1] = parametro;
-		argv[2] = ".";
-		argv[3] = NULL;
-
-		if (execve(argv[0], argv, NULL) == -1)
-		{
-			perror("Error");
-		}
+		return (builtin->f(info));
+	}
+	if (_strchr(*info->tokens, '/') == -1)
+	{
+		free_list(&info->path);
+		info->path = str_to_list(get_dict_val(info->env, "PATH"), ':');
+		info->exe = search_path(info, info->path);
 	}
 	else
 	{
-		o = find_command(vartoprint);
-
-		slash = str_concat(o, slash);
-
-		s = str_concat(slash, *cmd);
-
-		argv[0] = s;
-		argv[1] = parametro;
-		argv[2] = ".";
-		argv[3] = NULL;
-
-		if (execve(argv[0], argv, NULL) == -1)
-		{
-			perror("Error");
-		}
+		info->exe = _strdup(*info->tokens);
 	}
+	if (info->exe && access(info->exe, X_OK) == 0)
+	{
+		return (_execute(info));
+	}
+	if (info->exe)
+	{
+		perrorl_default(*info->argv, info->lineno, "Permission denied",
+				*info->tokens, NULL);
+		info->status = 126;
+	}
+	else
+	{
+		perrorl_default(*info->argv, info->lineno, "not found",
+				*info->tokens, NULL);
+		info->status = 127;
+	}
+	return (info->status);
 }
 
+
+/**
+ * _execute - fork and exec the current command
+ * @info: shell information
+ *
+ * Return: exit status of the child process
+ */
+int _execute(info_t *info)
+{
+	char *exe, **argv, **env;
+
+	switch (fork())
+	{
+	case 0:
+		exe = info->exe;
+		argv = info->tokens;
+		env = dict_to_env(info->env);
+
+		info->exe = NULL;
+		info->tokens = NULL;
+		free_info(info);
+
+		execve(exe, argv, env);
+		perror(*argv);
+
+		if (info->file)
+			close(info->fileno);
+
+		free(exe);
+		free_tokens(&argv);
+		free_tokens(&env);
+		exit(EXIT_FAILURE);
+		break;
+	case -1:
+		perrorl_default(*info->argv, info->lineno, "Cannot fork", NULL);
+		info->status = 2;
+		break;
+	default:
+		wait(&info->status);
+		info->status = WEXITSTATUS(info->status);
+		break;
+	}
+	free(info->exe);
+	info->exe = NULL;
+
+	return (info->status);
+}
